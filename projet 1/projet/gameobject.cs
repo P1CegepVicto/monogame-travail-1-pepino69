1495a5d5ae988a8f7c10e3f3731651f66cc5585a
pepino69 <pepepepin@live.ca>
2016-11-08 16:53:55 -0500

    premier envoi github
    rhnyd

---
+++ projet 1/projet/gameobject.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet
+{
+    class GameObject
+    {
+        public bool estvivant;
+        public Texture2D sprite;
+        public Vector2 position;
+        public Vector2 vitesse;
+
+    }
+}
